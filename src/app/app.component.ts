08ef89ff4c6f119597ee621eaf626a40ad21c647
veysel <vinanc72@gmail.com>
2019-11-29 00:37:29 +0300

    ol map module added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-map';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    let map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([11.067917, 55.696086]),
+        zoom: 5
+      })
+    });
+
+  }
+
 }
